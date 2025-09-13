4d0c45b16fd29b404e7d3902b50ceb05589c9dcf
Laurids <49726628+Jibs23@users.noreply.github.com>
2025-09-13 16:30:02 +0200

    Add speedrun timer

---
+++ scripts/speedrun_timer.gd
@@ -0,0 +1,19 @@
+extends Label
+
+var timer: float = 0.0
+var time_is_running: bool = false
+
+func _ready():
+	visible = false
+
+func start_timer() -> void:
+	time_is_running = true
+	visible = true
+
+func stop_timer() -> void:
+	time_is_running = false
+
+func _process(delta: float) -> void:
+	if time_is_running:
+		timer += 1*delta
+		text = str(timer).pad_decimals(2)
