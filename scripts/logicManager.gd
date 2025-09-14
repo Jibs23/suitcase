27187401da5e6a2ba3c422bd65827bba8633d303
Laurids <49726628+Jibs23@users.noreply.github.com>
2025-09-14 09:16:19 +0200

    Add main menu fix

--- scripts/logicManager.gd
+++ scripts/logicManager.gd
@@ -6,7 +6,16 @@ var audio_manager: Node
 var board: Node2D
 var menu: Control
 var speedrun_timer: Label
+var background: Sprite2D
 
 ## Returns true if an item is currently selected.
 func is_item_selected() -> bool:
 	return item_manager.selected_item != null
+
+func _ready():
+	print("item_manager: ", item_manager)
+	print("audio_manager: ", audio_manager)
+	print("board: ", board)
+	print("menu: ", menu)
+	print("speedrun_timer: ", speedrun_timer)
+	print("background: ", background)
